69bba30be4f4938f69c69ec05f7e3bb1ce564d14
MPalacio <emanuel.palacio@gmail.com>
2018-02-21 11:23:02 +0100

    Update build files

--- before-deploy.sh
+++ before-deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
+
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_9d12ac39910d_key -iv $encrypted_9d12ac39910d_iv -in codesigning.asc.enc -out codesigning.asc -d
+    openssl aes-256-cbc -K $encrypted_0c244a1263e9_key -iv $encrypted_0c244a1263e9_iv -in codesigning.asc.enc -out codesigning.asc -d
     gpg --fast-import --no-tty --yes codesigning.asc
 fi
