31b7c19926162aacd086630c8f98211b7f64e4ad
JGuerreiro <joao.guerreiro@cambio.se>
2018-09-18 13:37:42 +0200

    Added comment

--- src/main/java/org/gdl2/cdshooks/Link.java
+++ src/main/java/org/gdl2/cdshooks/Link.java
@@ -8,6 +8,10 @@ import lombok.Data;
 
 import java.net.URL;
 
+/* One of the requirements of cds-service, is to
+   be able to override a link's url,
+   thus Link class uses the @Data annotation instead of @Value
+ */
 @Data
 @Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
